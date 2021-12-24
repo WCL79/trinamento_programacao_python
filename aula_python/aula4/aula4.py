3ade92ef40c653c6feccb2a6c336cd7f1236c6bf
Weslley Candido Lima <wclima5@stefanini.com>
2021-12-24 09:28:50 -0300

    Aula 4 sobre Operação de CAST, Função de Type...

--- aula_python/aula4/aula4.py
+++ aula_python/aula4/aula4.py
@@ -1,5 +1,8 @@
-x=1
-x="C"
+x=1 #inteiro
+x="C" #String
+x=15.6 #float
+x=False #boolean
 
+'''str = Operação de CAST e a função type retorna a tipagem'''
 print("Valor :"+str(x))
 print("Valor :"+str(type(x)))
